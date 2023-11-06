5f103bc1e96b6665be79abf50d85094cdd834f43
ImamNurhadi <adigaming015@mail.com>
2023-11-06 13:22:48 +0700

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,59 @@
-<script setup>
+<template>
+  <div>
+    <form @submit.prevent="hitung">
+      <input v-model="nomorsatu" type="number" placeholder="Masukkan angka pertama">
+      <input v-model="nomordua" type="number" placeholder="Masukkan angka kedua">
+      <select v-model="operator">
+        <option value="penjumlahan">+</option>
+        <option value="pengurangan">-</option>
+        <option value="perkalian">*</option>
+        <option value="pembagian">/</option>
+        <option value="pangkat">^</option>
+      </select>
+      <button type="submit">Hitung</button>
+    </form>
+
+    <div v-if="hasil !== null">
+      <h2>Hasil:</h2>
+      <p>{{ hasil }}</p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      nomorsatu: "",
+      nomordua: "",
+      operator: "",
+      hasil: null,
+    };
+  },
+
+  methods: {
+    hitung() {
+
+      const surprise = "Hitung Sendiri";
+      if (this.operator === "penjumlahan") {
+        this.hasil = parseFloat(this.nomorsatu) + parseFloat(this.nomordua);
+      } else if (this.operator === "pengurangan") {
+        this.hasil = parseFloat(this.nomorsatu) - parseFloat(this.nomordua);
+      } else if (this.operator === "perkalian") {
+        this.hasil = parseFloat(this.nomorsatu) * parseFloat(this.nomordua);
+      } else if (this.operator === "pembagian") {
+        this.hasil = parseFloat(this.nomorsatu) / parseFloat(this.nomordua);
+      }
+       else if (this.operator ==="pangkat")
+       {
+        this.hasil = surprise;
+       }
+    },
+  },
+};
+</script>
+
+<!-- <script setup>
 import HelloWorld from './components/HelloWorld.vue'
 import TheWelcome from './components/TheWelcome.vue'
 </script>
@@ -44,4 +99,4 @@ header {
     flex-wrap: wrap;
   }
 }
-</style>
+</style> -->
